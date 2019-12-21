3380a0b0fdb49afb09714707062d8dbc21eb2241
tiffany6022 <tiffany001510@gmail.com>
2019-12-21 23:20:29 +0800

    feat: use glob and AttributeError

--- identify.py
+++ identify.py
@@ -1,35 +1,37 @@
 import os
 import shutil
-from facenet.upload import compare
+import glob
+from upload import compare
 
 
 def foldercompare(video):
     IMG_FOLDER = video
-    path_list = os.listdir(IMG_FOLDER)
+    # path_list = os.listdir(IMG_FOLDER)
+    path_list = glob.glob(r'./results/*.jpg')
+    # for py in path_list:
+    #     print(py)
 
-    newdir_path = os.path.join(IMG_FOLDER, "09chen")
+    newdir_path = os.path.join("./", "09chen")
     os.mkdir(newdir_path)
 
-    for img2 in path_list:
-        img2_path = os.path.join(IMG_FOLDER, img2)
-        if img2.startswith('.'):
-            continue
-        if not os.path.isfile(img2_path):
-            continue
-        if not img1 == img2:
-            continue
+    for img2_path in path_list:
+        # img2_path = os.path.join(IMG_FOLDER, img2)
+        # if img2.startswith('.'):
+        #     continue
+        # if not os.path.isfile(img2_path):
+        #     continue
+        # if img1 == img2:
+        #     continue
         try:
             diff = compare(img1_path, img2_path)
-        except KeyboardInterrupt:
-            os._exit()
-        except:
-            print("can't:", img2)
+        except AttributeError:
+            print("can't:", img2_path)
             continue
         if diff < 0.9:
             shutil.move(img2_path, newdir_path)
-            print("img2:", img2)
+            print("img2:", img2_path)
 
-    shutil.move(img1_path, newdir_path)
+    # shutil.move(img1_path, newdir_path)
     print("end:\n", video)
 
 img1 = "081209934_person_0.jpg"
